6f71edf2f8d2cce6856fc2ad083c6d57ee9c38d2
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-01-22 21:34:23 -0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -7,7 +8,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PaisModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
