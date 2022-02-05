7a46a420f8ede22e2c56c00af9b0adce2dae9970
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-02-04 23:07:21 -0300

    feat: add input to see countrys in table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -8,7 +9,13 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PaisModule,
+    SharedModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private PaisService: PaisService) {}
 
-  ngOnInit(): void {
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+    this.PaisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }
